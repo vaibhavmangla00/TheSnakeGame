2ee0b67704075f74eae6b5cc7c6c309b8223d66f
Vaibhav Mangla <vaibhavmangla00@gmail.com>
2022-04-28 15:54:45 +0530

    data.txt is added

--- SnakeGame with Maintaining a High Score/scoreboard.py
+++ SnakeGame with Maintaining a High Score/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open('data.txt') as file:
+            self.highscore = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open('data.txt', 'w') as file:
+                file.write(f'{self.highscore}')
         self.score = 0
         self.update_scoreboard()
 
